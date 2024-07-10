a57c173a6b11a1bd04a16024188657f5af631308
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2024-07-10 09:40:07 +0700

    feat: make createAccount api with global exception handler

--- accounts/src/main/java/com/eazybytes/accounts/service/IAccountService.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/IAccountService.java
@@ -3,5 +3,9 @@ package com.eazybytes.accounts.service;
 import com.eazybytes.accounts.dto.CustomerDto;
 
 public interface IAccountService {
+	
+	/**
+	 * @param customerDto - CustomerDto Object
+	 */
 	void createAccount(CustomerDto customerDto);
 }
--- accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountServiceImpl.java
@@ -1,11 +1,20 @@
 package com.eazybytes.accounts.service.impl;
 
+import com.eazybytes.accounts.constants.AccountConstants;
 import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Account;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.exception.CustomerAlreadyExistsException;
+import com.eazybytes.accounts.mapper.CustomerMapper;
 import com.eazybytes.accounts.repository.AccountRepository;
 import com.eazybytes.accounts.repository.CustomerRepository;
 import com.eazybytes.accounts.service.IAccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -14,8 +23,41 @@ public class AccountServiceImpl implements IAccountService {
 	private AccountRepository accountRepository;
 	private CustomerRepository customerRepository;
 	
+	/**
+	 * @param customerDto - CustomerDto Object
+	 */
 	@Override
 	public void createAccount(CustomerDto customerDto) {
+		// Create Customer
+		Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+		Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customer.getMobileNumber());
+		if (optionalCustomer.isPresent()) {
+			throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
+				+ customer.getMobileNumber());
+		}
+		customer.setCreatedAt(LocalDateTime.now());
+		customer.setCreatedBy("Anonymous");
+		Customer savedCustomer = customerRepository.save(customer);
+		
+		// Create Account
+		accountRepository.save(createNewAccount(savedCustomer));
+	}
 	
+	/**
+	 * @param customer - Customer Object
+	 * @return the new account details
+	 */
+	private Account createNewAccount(Customer customer) {
+		Account newAccount = new Account();
+		newAccount.setCustomerId(customer.getCustomerId());
+		long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+		
+		newAccount.setAccountNumber(randomAccNumber);
+		newAccount.setAccountType(AccountConstants.SAVINGS);
+		newAccount.setBranchAddress(AccountConstants.ADDRESS);
+		newAccount.setCreatedAt(LocalDateTime.now());
+		newAccount.setCreatedBy("Anonymous");
+		
+		return newAccount;
 	}
 }
